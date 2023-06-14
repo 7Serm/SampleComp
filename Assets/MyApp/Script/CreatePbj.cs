e2382e41a7b155163539bed3b3433260cca26970
7seern <mayamaya521999@gmail.com>
2023-06-15 00:56:56 +0900

    ランダムにオブジェクトを配置できるかつ同じ場所には生成しないようにした

--- Assets/MyApp/Script/CreatePbj.cs
+++ Assets/MyApp/Script/CreatePbj.cs
@@ -6,17 +6,25 @@ using UnityEngine;
 public class CreatePbj : MonoBehaviour
 {
 
-    [SerializeField] private Transform[] CreatePoint;
-    [SerializeField] private GameObject[] Gameobj;
+  //  [SerializeField] private Transform[] CreatePoint;
+  //  [SerializeField] private GameObject[] Gameobj;
+    [SerializeField] private List<GameObject> GameObjects;
+    [SerializeField] private List<Transform> CreateP;
 
     private int Randnum;
+    private int Lcount;
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < CreatePoint.Length; i++)
+        Lcount = CreateP.Count;
+        int l = Lcount;
+        for(int i = 0; i <  l; i++)
         {
-           Randnum =  Random.Range(0, 5);
-            Instantiate(Gameobj[i], CreatePoint[Randnum]);
+           Randnum =  Random.Range(0, Lcount);
+            Instantiate(GameObjects[i], CreateP[Randnum]);
+           // GameObjects.RemoveAt(i);
+            CreateP.RemoveAt(Randnum);
+            Lcount = CreateP.Count;
         }
     }
 
