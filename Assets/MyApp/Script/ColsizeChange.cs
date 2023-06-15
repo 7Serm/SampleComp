495345184ec4d9b6964a095f017157f61fac904f
seno0010 <mayamaya521999@gmail.com>
2023-06-15 16:08:56 +0900

    コライダーのサイズ変更スクリプトにコードコメントの追加を行った

--- Assets/MyApp/Script/ColsizeChange.cs
+++ Assets/MyApp/Script/ColsizeChange.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 public class ColsizeChange : MonoBehaviour
 {
 
-    private CapsuleCollider Col;
+    private CapsuleCollider Col;  // カプセルコライダーのコンポーネントを入れるための変数
 
-    [SerializeField] private float y;
+    [SerializeField] private float y;　// ここはセンターアイアンカーに入れ替わる変数　本来はGameObjectをアタッチして　transformのyの値を取得のために使う
 
-    private Vector3 v3;
+    private Vector3 v3;　　//ベクター３型の変数　
     // Start is called before the first frame update
     void Start()
     {
-        Col = GetComponent<CapsuleCollider>();
-        
-        
+        Col = GetComponent<CapsuleCollider>();　　//Colにカプセルコライダーコンポーネントを取得させている
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        v3 = new Vector3(0, y / 2, 0);
-        Col.height = y;
-        Col.center = v3;
+        v3 = new Vector3(0, y / 2, 0);　　// ｖ3に　ｘは0　ｙは高さの半分を　ｚは0　入れている
+        Col.height = y;　　　　　　　　　　//高さにはセンターアイアンカーの位置を入れるようにする
+        Col.center = v3;　　　　　　　　　// センターのｙに高さの半分位置に移動いるようにしている
     }
 }
